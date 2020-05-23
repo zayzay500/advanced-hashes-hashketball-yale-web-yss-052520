e02dcd96c89ca930444cbeede9ce5eb0e3575a10
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-23 01:16:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -233,6 +233,13 @@ def player_stats(player_name)
 end
 
 def big_shoe_rebounds
+  biggest_shoe = ""
+  
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      binding.pry
+    end
+  end
 end
 
 
